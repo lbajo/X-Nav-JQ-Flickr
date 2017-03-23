7d4adad98ec62ac24e2951ab944218ea418b33b5
Lorena <lbajo9@gmail.com>
2017-03-23 19:18:35 +0100

    final

--- flickr.js
+++ flickr.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
     $("#search").click(function(){
     	var value = $("#text").val();
-    	$.getJSON( "http://api.flickr.com/services/feeds/photos_public.gne?tags="+value+"&tagmode=any&format=json&jsoncallback=?")
-    	.done(function(data) {
+    	var flickerAPI="http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    	$.getJSON( flickerAPI, {
+    		tags: value,
+    		tagmode: "any",
+    		format: "json"
+  		}).done(function(data) {
         	$.each(data.items, function(i,item) {
           		$("<img>").attr("src", item.media.m).appendTo("#images");
         	});
 		});
     });
     $("#searchF").click(function(){
-    	//var value = $("#text").val();
-    	$.getJSON( "http://api.flickr.com/services/feeds/photos_public.gne?tags=fuenlabrada&tagmode=any&format=json&jsoncallback=?")
-    	.done(function(data) {
+    	var flickerAPI="http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    	$.getJSON( flickerAPI, {
+    		tags: "fuenlabrada",
+    		tagmode: "any",
+    		format: "json"
+  		}).done(function(data) {
         	$.each(data.items, function(i,item) {
           		$("<img>").attr("src", item.media.m).appendTo("#images");
         	});
