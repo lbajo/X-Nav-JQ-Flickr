ef0155b085415e19329131885e6c3b4d4e79a642
Lorena <lbajo9@gmail.com>
2017-03-23 18:59:12 +0100

    fin

--- flickr.js
+++ flickr.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
     $("#search").click(function(){
     	var value = $("#text").val();
-    	//$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+value+"&tagmode=any&format=json&jsoncallback=?")
-    	$.getJSON( "http://api.flickr.com/services/feeds/photos_public.gne?tags="+value+"&tagmode=any&format=json&jsoncallback=?", {
-      		tagmode: "any",
-      		format: "json"
-		}).done(function(data) {
+    	$.getJSON( "http://api.flickr.com/services/feeds/photos_public.gne?tags="+value+"&tagmode=any&format=json&jsoncallback=?")
+    	.done(function(data) {
         	$.each(data.items, function(i,item) {
           		$("<img>").attr("src", item.media.m).appendTo("#images");
         	});
