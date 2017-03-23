df9aeccfc31d31076f147f5371bc6a890077ca66
Lorena <lbajo9@gmail.com>
2017-03-23 18:21:01 +0100

    Flickr

---
+++ flickr.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#search").click(function(){
+    	var value = $("#text").val();
+    	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+value+"&tagmode=any&format=json&jsoncallback=?")
+    	 .done(function(data) {
+        	$.each(data.items, function(i,item) {
+          		$("<img>").attr("src", item.media.m).appendTo("#images");
+        	});
+		});
+    });
+});
